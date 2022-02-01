f0de440d8c72e1a9099d7a9a233a741f9cb2b0b6
Eilis Sutton <G00411335@gmit.ie>
2022-02-01 20:17:42 +0000

    lecture code for week 02

---
+++ Week02/helloName.py
@@ -0,0 +1,11 @@
+# Uses a variable to greet
+# Author: Éilis Sutton
+
+name = "Éilis"
+print ('hello ' + name)
+
+# this won't work
+age = 34
+#print ('Your age is ' + age)
+print ('your age is ' +str(age))
+print ('Hello {} \tYour age is {}'.format (name, age))
